0f85697a4d4a427c42afb052327d46f987449e38
GalaxyNova7 <sarmad_nmj@hotmail.com>
2025-02-21 17:50:44 +0100

    varible string

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,8 @@ public class Main {
     public static void main(String[] args){
       int unm1 = 15;
         System.out.println(unm1);
+      String unm3 = "sonic";
+        System.out.println(unm3);
+
     }
 }
