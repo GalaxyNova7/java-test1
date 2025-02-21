7ec7aa262beef7365fe03450b6d1df1d52676da8
GalaxyNova7 <sarmad_nmj@hotmail.com>
2025-02-21 18:23:02 +0100

    user profail

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,30 @@
+import java.sql.SQLOutput;
+
 public class Main {
     public static void main(String[] args){
+      /*
       int unm1 = 15;
         System.out.println(unm1);
       String unm3 = "sonic";
         System.out.println(unm3);
 
+
+
+        int x = 55;
+
+        System.out.println(x);
+
+        String U = "Go";
+
+        System.out.println(U);
+
+
+       */
+
+        String Nume ="YOOOu";
+        String Email ="kfghskfshgk292@yahoo.com";
+        int age = 31;
+        String adress = "UFO55";
+        System.out.println(Nume +" " + Email +" " + age + " " + adress);
     }
 }
